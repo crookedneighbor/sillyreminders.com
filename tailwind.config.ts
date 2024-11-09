aa5e6c817bed978bab2d4283a9a32c1d9a9d25ce
Blade Barringer <blade@crookedneighbor.com>
2024-11-09 10:30:06 -0600

    feat: set up color scheme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,14 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
+		colors: {
+			yellow: '#DACC3E',
+			purple: '#57467B',
+			red: '#BC2C1A',
+			blue: '#07A0C3',
+			black: '#1F271B',
+			cream: '#FAF0CA'
+		},
 		extend: {}
 	},
 
