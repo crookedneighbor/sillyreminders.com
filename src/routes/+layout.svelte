d34256d1d97a34f51fa9710e536fcc087e7ef733
Blade Barringer <blade@crookedneighbor.com>
2024-11-09 10:40:48 -0600

    feat: create layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,9 @@
 <div class="wrapper">
 	<h1>SillyReminders.com</h1>
 
-	{@render children()}
+	<div class="page">
+		{@render children()}
+	</div>
 </div>
 
 <style lang="postcss">
@@ -16,16 +18,24 @@
 
 	.wrapper {
 		background-color: theme(colors.yellow);
-		@apply prose m-4 mx-auto p-4;
+		@apply prose m-4 rounded-xl p-4;
+	}
+
+	.page {
+		background-color: theme(colors.cream);
+		@apply rounded p-4 pt-6;
 	}
 
 	h1 {
-		@apply text-center;
+		background-color: theme(colors.cream);
+		/* counteract default by prose class */
+		margin-bottom: -1rem !important;
+		@apply m-auto w-min rounded-t-xl px-8 pb-0 pt-2 text-center;
 	}
 
 	@media screen(sm) {
 		.wrapper {
-			@apply prose-xl;
+			@apply prose-xl mx-auto;
 		}
 	}
 </style>
