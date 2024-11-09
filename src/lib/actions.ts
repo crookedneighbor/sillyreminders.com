02aaf1296ff18c8b8ce214255d0dbd0bda15cc9e
Blade Barringer <blade@crookedneighbor.com>
2024-11-09 11:16:28 -0600

    feat: add actions & hows

---
+++ src/lib/actions.ts
@@ -0,0 +1,19 @@
+export const actions = {
+	read: {
+		message: 'read'
+	},
+	draw: {
+		message: 'draw a squirrel'
+	},
+	eat: {
+		message: 'eat a meal'
+	},
+	bike: {
+		message: 'ride a bike'
+	},
+	walk: {
+		message: 'walk'
+	}
+};
+
+export const actionParams = Object.keys(actions);
---
+++ src/lib/hows.ts
@@ -0,0 +1,19 @@
+export const hows = {
+	nose: {
+		message: 'with your nose'
+	},
+	head: {
+		message: 'with your head'
+	},
+	bones: {
+		message: 'with your bones'
+	},
+	toys: {
+		message: 'while playing with toys'
+	},
+	kitty: {
+		message: 'with a kitty'
+	}
+};
+
+export const howParams = Object.keys(hows);
