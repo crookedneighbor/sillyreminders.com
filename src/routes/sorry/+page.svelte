f2341e5d0ce75be4ef2920b29d915550393ea3c5
Blade Barringer <blade@crookedneighbor.com>
2024-11-10 10:41:04 -0600

    feat: add sorry page

---
+++ src/routes/sorry/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import random from '$lib/random';
+
+	function reset() {
+		localStorage.setItem('used-paths', '');
+		random();
+	}
+</script>
+
+<div class="page">
+	<h2 class="text-center">Whoops! Looks like you are out of reminders!</h2>
+	<button onclick={reset}>Reset!</button>
+</div>
+
+<style lang="postcss">
+	.page {
+		background-color: theme(colors.cream);
+		@apply rounded p-4 pt-6;
+	}
+
+	h2 {
+		@apply my-8 text-3xl;
+	}
+
+	button {
+		@apply m-auto block rounded bg-blue px-16 py-4 text-8xl text-cream transition-colors;
+	}
+	button:hover {
+		@apply bg-red;
+	}
+</style>
