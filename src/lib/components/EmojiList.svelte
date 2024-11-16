343b8040a6972149af29201309104dad4f0f9503
Blade Barringer <blade@crookedneighbor.com>
2024-11-16 14:21:25 -0600

    feat: add emoji route

---
+++ src/lib/components/EmojiList.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	interface Props {
+		title: string;
+		emojis: { emoji: string; animation?: string }[];
+	}
+	let { title, emojis }: Props = $props();
+</script>
+
+<h2 class="mt-8">{title}</h2>
+<div class="mb-8 grid grid-cols-3 gap-3">
+	{#each emojis as emoji}
+		<div class="my-8 w-full text-center text-9xl">
+			<div class={`animate-${emoji.animation || 'wiggle'}`}>
+				{emoji.emoji}
+			</div>
+		</div>
+	{/each}
+</div>
+
+<style lang="postcss">
+	h2 {
+		@apply text-5xl;
+	}
+</style>
---
+++ src/routes/emojis/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { actions } from '$lib/actions';
+	import EmojiList from '$lib/components/EmojiList.svelte';
+	import { hows } from '$lib/hows';
+	import { things } from '$lib/things';
+
+	const actionEmojis = $derived(Object.values(actions));
+	const howEmojis = $derived(Object.values(hows));
+	const thingEmojis = $derived(Object.values(things));
+</script>
+
+<EmojiList emojis={actionEmojis} title="Actions" />
+<EmojiList emojis={howEmojis} title="Hows" />
+<EmojiList emojis={thingEmojis} title="Things" />
