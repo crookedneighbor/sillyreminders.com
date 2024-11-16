65b6324ac34443e195d301979a6b589ebbe54b5f
Blade Barringer <blade@crookedneighbor.com>
2024-11-16 14:46:55 -0600

    feat: center emoji text

--- src/lib/components/EmojiList.svelte
+++ src/lib/components/EmojiList.svelte
@@ -6,7 +6,7 @@
 	let { title, emojis }: Props = $props();
 </script>
 
-<h2 class="mt-8">{title}</h2>
+<h2 class="mt-8 text-center">{title}</h2>
 <div class="mb-8 grid grid-cols-3 gap-3">
 	{#each emojis as emoji}
 		<div class="my-8 w-full text-center text-9xl">
